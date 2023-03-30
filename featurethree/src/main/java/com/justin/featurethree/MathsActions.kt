8f737c7608620b0492269aa44eb75c143494c1c9
Justin George <justin.george@swapcard.com>
2023-03-30 21:54:08 +0530

    feature four

--- featurethree/src/main/java/com/justin/featurethree/MathsActions.kt
+++ featurethree/src/main/java/com/justin/featurethree/MathsActions.kt
@@ -1,5 +1,7 @@
 package com.justin.featurethree
 
+import android.util.Log
+
 class MathsActions {
 
     fun sumTwoValues(a: Int, b: Int) = a + b
@@ -14,4 +16,12 @@ class MathsActions {
      fun divTwoValues(a: Int, b: Int) = a / b
 
      fun divsTwoValues(a: Int, b: Int) = a / b
+
+    fun checkValueIsGreater(a: Int, b: Int) = if (a<b){
+        Log.d("TAG_JUSTIN","VALUES")
+        true
+    } else{
+        Log.d("TAG_JUSTIN","VALUES")
+        false
+    }
 }
