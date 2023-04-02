b3e1b28c390a7889ccdf7f39c46f2228cde2ef43
Justin George <justin.george@swapcard.com>
2023-04-02 20:21:41 +0530

    changes

---
+++ app/src/main/java/com/justin/codecoveragetest/ActionsTwo.kt
@@ -0,0 +1,17 @@
+package com.justin.codecoveragetest
+
+class ActionsTwo {
+
+    fun sumTwoValues(a: Int, b: Int) = a + b
+
+    fun minusTwoValues(a: Int, b: Int) = a - b
+
+     fun multiplyTwo(a: Int, b: Int) = multiplyTwoValues(a,b)
+
+
+    private fun multiplyTwoValues(a: Int, b: Int) = a * b
+
+     fun divTwoValues(a: Int, b: Int) = a / b
+
+     fun divsTwoValues(a: Int, b: Int) = a / b
+}
