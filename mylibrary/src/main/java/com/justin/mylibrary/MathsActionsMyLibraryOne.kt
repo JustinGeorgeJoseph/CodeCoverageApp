7cbf6b47664d446d3304a935ef4f618fbe26bce4
Justin George <justin.george@swapcard.com>
2023-04-03 12:08:29 +0530

    add changes

---
+++ mylibrary/src/main/java/com/justin/mylibrary/MathsActionsMyLibraryOne.kt
@@ -0,0 +1,30 @@
+package com.justin.mylibrary
+
+class MathsActionsMyLibraryOne {
+
+    fun sumTwoValues(a: Int, b: Int) = a + b
+
+    fun minusTwoValues(a: Int, b: Int) = a - b
+
+     fun multiplyTwo(a: Int, b: Int) = multiplyTwoValues(a,b)
+
+
+    private fun multiplyTwoValues(a: Int, b: Int) = a * b
+
+     fun divTwoValues(a: Int, b: Int) = a / b
+
+     fun divTwoValues1(a: Int, b: Int) = a / b
+
+     fun divTwoValues2(a: Int, b: Int) = a / b
+
+     fun divTwoValues3(a: Int, b: Int) = a / b
+
+
+    fun divTwoValues11(a: Int, b: Int) = a / b
+
+    fun divTwoValues22(a: Int, b: Int) = a / b
+
+    fun divTwoValues33(a: Int, b: Int) = a / b
+
+     fun divsTwoValues(a: Int, b: Int) = a / b
+}
